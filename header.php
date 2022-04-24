a87a9f9cec27a5430f2c458bc969c6dec70ebb59
sajed <sajedaiub@gmail.com>
2022-04-24 11:59:48 +0600

    some update

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
         <div class="col-sm-3 sidenav">
             <h4><a href="">CRUD Generator</a></h4>
             <ul class="nav nav-pills nav-stacked">
-                <li class="active"><a href="">Home</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
                 <?php if( isset($_COOKIE["download_file_src"]) ) { ?>
                    <li><a href="<?php echo $_COOKIE['download_file_src']; ?>">Download Code</a></li>
                 <?php } ?>
